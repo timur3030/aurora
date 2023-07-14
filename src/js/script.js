92ad48236b105595fec5cce1597ed58f30d64cc0
Timur <timur87@mail.ru>
2023-07-14 04:02:36 +0300

    finished

--- src/js/script.js
+++ src/js/script.js
@@ -1,56 +1,58 @@
-const modal = document.querySelector(".modal"),
-  formBtn = document.querySelector(".form__btn"),
-  userName = document.querySelector("#name"),
-  userTel = document.querySelector("#tel"),
-  hamburger = document.querySelector(".hamburger"),
-  menu = document.querySelector(".header__menu"),
-  menuList = document.querySelectorAll(".header__menu-link"),
-  promoImg = document.querySelector(".promo__img"),
-  header = document.querySelector(".header");
+document.addEventListener("DOMContentLoaded", () => {
+  const modal = document.querySelector(".modal"),
+    formBtn = document.querySelector(".form__btn"),
+    userName = document.querySelector("#name"),
+    userTel = document.querySelector("#tel"),
+    hamburger = document.querySelector(".hamburger"),
+    menu = document.querySelector(".header__menu"),
+    menuList = document.querySelectorAll(".header__menu-link"),
+    promoImg = document.querySelector(".promo__img"),
+    header = document.querySelector(".header");
 
-window.addEventListener("scroll", function () {
-  if (
-    document.documentElement.scrollTop + 96 >
-    window.getComputedStyle(promoImg).height.replace(/px/g, "")
-  ) {
-    header.classList.add("header_color");
-  } else {
-    header.classList.remove("header_color");
-  }
-});
-
-hamburger.addEventListener("click", () => {
-  menu.classList.toggle("active");
-  hamburger.classList.toggle("hamburger_active");
-});
-
-menuList.forEach((item) => {
-  item.addEventListener("click", () => {
-    menu.classList.remove("active");
-    hamburger.classList.remove("hamburger_active");
+  window.addEventListener("scroll", function () {
+    if (
+      document.documentElement.scrollTop + 96 >
+      window.getComputedStyle(promoImg).height.replace(/px/g, "")
+    ) {
+      header.classList.add("header_color");
+    } else {
+      header.classList.remove("header_color");
+    }
   });
-});
 
-formBtn.addEventListener("click", () => {
-  if (userName.value && userTel.value) {
-    modal.classList.add("activate");
-    setTimeout(() => {
-      modal.classList.remove("activate");
-    }, 2000);
-  }
-});
+  hamburger.addEventListener("click", () => {
+    menu.classList.toggle("active");
+    hamburger.classList.toggle("hamburger_active");
+  });
 
-$(document).ready(function () {
-  $("form").submit(function (e) {
-    e.preventDefault();
-    $.ajax({
-      type: "POST",
-      url: "mailer/smart.php",
-      data: $(this).serialize(),
-    }).done(function () {
-      $(this).find("input").val("");
-      $("form").trigger("reset");
+  menuList.forEach((item) => {
+    item.addEventListener("click", () => {
+      menu.classList.remove("active");
+      hamburger.classList.remove("hamburger_active");
     });
-    return false;
+  });
+
+  formBtn.addEventListener("click", () => {
+    if (userName.value && userTel.value) {
+      modal.classList.add("activate");
+      setTimeout(() => {
+        modal.classList.remove("activate");
+      }, 2000);
+    }
+  });
+
+  $(document).ready(function () {
+    $("form").submit(function (e) {
+      e.preventDefault();
+      $.ajax({
+        type: "POST",
+        url: "mailer/smart.php",
+        data: $(this).serialize(),
+      }).done(function () {
+        $(this).find("input").val("");
+        $("form").trigger("reset");
+      });
+      return false;
+    });
   });
 });
